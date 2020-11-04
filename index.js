064d58b7153739e1288de0a60025f6889f2df16a
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-11-04 15:01:52 +0300

    removed cookies Parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const setUpRoutes = require("./models/routes");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
+//const cookieParser = require("cookie-parser");
 
 const start = async () => {
   try {
@@ -20,7 +20,7 @@ const start = async () => {
 
     app.use(cors());
     app.use(express.json());
-    app.use(cookieParser());
+    //app.use(cookieParser());
 
     setUpRoutes(app);
     console.log("app routes is set up lets listen to the port ");
