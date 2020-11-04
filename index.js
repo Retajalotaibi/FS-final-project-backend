2ec66c602a8dcd86c57e3e636b3b1a761b0dbbf7
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-11-04 14:30:59 +0300

    mongodb

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
- const express = require("express");
-  const mongoose = require("mongoose");
- const setUpRoutes = require("./models/routes");
- const cors = require("cors");
- const cookieParser = require("cookie-parser")
- 
-
+const express = require("express");
+const mongoose = require("mongoose");
+const setUpRoutes = require("./models/routes");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Aziz:Aziz1234@cluster0.ynpi5.mongodb.net/Final-project-barmej?authSource=admin&replicaSet=atlas-hh9b4k-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+    console.log("Connected to mongoDB");
 
-try {
-  await mongoose.connect("mongodb://localhost/Final-project-barmej",{
-  useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-})
-console.log("Connected to mongoDB")
+    const app = express();
 
-  const app = express();
-  
+    app.use(cors());
+    app.use(express.json());
+    app.use(cookieParser());
 
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
+    setUpRoutes(app);
+    console.log("app routes is set up lets listen to the port ");
 
-  setUpRoutes(app)
-  console.log("app routes is set up lets listen to the port ")
+    app.listen(4000);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-app.listen(4000);
-} catch (error) {
-  console.error(error)
-}
-
-}
-
-start()
-
-
-
+start();
