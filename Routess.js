ecddb280c8c4f83fa9d209568af2c9f189696020
jawanAlhulail <Jawanalhulail@gmail.com>
2020-11-22 00:31:13 +0300

    Controllers are done

---
+++ Routess.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const Home = require("./Controllers/Home");
+const Register = require("./Controllers/Register");
+const Login = require("./Controllers/Login");
+
+router.get("/", Home);
+router.post("/register", Register);
+router.post("/login", Login);
+
+module.exports = router;
