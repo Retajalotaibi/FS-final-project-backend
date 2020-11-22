e2aba6e86fc190e5860b58483f9752aeacf5a448
Retajalotaibi <63907341+Retajalotaibi@users.noreply.github.com>
2020-11-22 23:01:35 +0300

    done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Home = require("./Controllers/Home");
-const Register = require("./Controllers/Register");
-const Login = require("./Controllers/Login");
+const Home = require("../Controllers/Home");
+const Register = require("../Controllers/Register");
+const Login = require("../Controllers/Login");
 
 router.get("/", Home);
 router.post("/register", Register);
